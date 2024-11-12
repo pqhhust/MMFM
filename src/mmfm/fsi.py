64bde1a4feba37ca88f57734ba9d7cd4c28f0ccf
martin.rohbeck <martin.rohbeck@quantco.com>
2024-11-12 14:14:46 +0100

    Combined fsi and fsi-utils

--- src/mmfm/fsi.py
+++ src/mmfm/fsi.py
@@ -66,7 +66,10 @@ class FSI:
                         found_valid_step = False
                         for next_existing_timestep in all_times[k + 1 :]:
                             k_timestep_next = all_times.index(next_existing_timestep)
-                            if not (any(np.isnan(y[idx, k_timestep_next])) & np.isnan(X[idx, k_timestep_next]).any()):
+                            if not (
+                                any(np.isnan(y[idx, k_timestep_next]))
+                                & np.isnan(X[idx, k_timestep_next]).any()
+                            ):
                                 found_valid_step = True
                                 break
                             # We will have left the previous loop if the final timepoint has only missings
@@ -130,7 +133,11 @@ class FSI:
         splines = []
         for i in range(X.shape[0]):
             # build the splines based on top of all time steps I saw during training
-            nonnans = [idx for idx, _ in enumerate(self.all_times) if not np.isnan(Xs[i, idx]).any()]
+            nonnans = [
+                idx
+                for idx, _ in enumerate(self.all_times)
+                if not np.isnan(Xs[i, idx]).any()
+            ]
             splines.append(
                 CubicSpline(
                     self.t_anchors[i][nonnans],
@@ -193,7 +200,9 @@ class FSI:
                 )
                 trajectory[tx, idx] = transport_c
 
-        color_classes = [int(x) for x in range(len([x for x in np.unique(y) if np.isfinite(x)]))]
+        color_classes = [
+            int(x) for x in range(len([x for x in np.unique(y) if np.isfinite(x)]))
+        ]
         colors = color_picker(color_classes)
         non_nan_targets = [x for x in np.unique(y) if np.isfinite(x)]
         for k, c in enumerate(non_nan_targets):
@@ -220,7 +229,9 @@ class FSI:
             )
             axidx.set_title(f"c={c}")
 
-        condition_to_class = {c: i for i, c in enumerate([x for x in np.unique(y) if np.isfinite(x)], 1)}
+        condition_to_class = {
+            c: i for i, c in enumerate([x for x in np.unique(y) if np.isfinite(x)], 1)
+        }
         # marginal_timepoints = np.linspace(0, len(trajectory) - 1, n_marginals).astype(int)
         marginal_timepoints = [int(x) for x in (np.unique(t) * (len(trajectory) - 1))]
         for n, idx in enumerate(idx_plot):
@@ -296,3 +307,276 @@ class FSI:
 
         with open(filename, "wb") as f:
             cloudpickle.dump(self.monge_maps, f)
+
+
+def fsi_derive_monge_maps(
+    X_train: dict, y_train: dict = None, t_train: list = None, conditional: bool = False
+):
+    """Derive Monge maps between marginals.
+
+    Args:
+        X_train (dict): Dictionary containing the training data for each time step.
+                        Shape: (n_samples, n_timepoints, n_features)
+        y_train (dict): Dictionary containing the training labels for each time step.
+                        Shape: (n_samples, n_timepoints)
+        t_train (list, np.ndarray): List of time points.
+        conditional (bool): Whether to use conditional OT maps.
+    """
+    monge_maps = Dict()
+    if conditional:
+        all_classes = sorted([int(x) for x in np.unique(y_train) if np.isfinite(x)])
+    all_times = sorted(np.unique(t_train))
+
+    # Learn Monge maps between marginals
+    # Barycentric Mapping
+    if not conditional:
+        for k, time in enumerate(all_times):
+            if k == (len(all_times) - 1):
+                break
+            ot_emd = ot.da.EMDTransport()
+            ot_emd.fit(Xs=X_train[:, k], Xt=X_train[:, k + 1])
+            monge_maps[time] = ot_emd
+
+    else:
+        for c in all_classes:
+            for k, time in enumerate(all_times):
+                if k == (len(all_times) - 1):
+                    break
+                idx = y_train[:, k] == c
+                if not all(np.isnan(y_train[idx, k])):
+                    for next_existing_timestep in all_times[k + 1 :]:
+                        k_timestep_next = all_times.index(next_existing_timestep)
+                        if not all(np.isnan(y_train[idx, k_timestep_next])):
+                            break
+
+                    # for next_existing_timestep in all_timesteps[k + 1 :]:
+                    ot_emd = ot.da.EMDTransport(limit_max=1e6)
+                    if np.isnan(X_train[idx, k]).any():
+                        print("Nan in X_train")
+                        raise ValueError("Nan in X_train")
+                    if np.isnan(X_train[idx, k_timestep_next]).any():
+                        print("Nan in X_train")
+                        raise ValueError("Nan in X_train")
+
+                    ot_emd.fit(Xs=X_train[idx, k], Xt=X_train[idx, k_timestep_next])
+                    monge_maps[time][c] = ot_emd
+                else:
+                    monge_maps[time][c] = None
+
+    return monge_maps.to_dict()
+
+
+def fsi_apply_monge_maps(
+    monge_maps, X_test, y_test=None, t_test=None, conditional=False, interpolate=True
+):
+    """Apply Monge maps to test data.
+
+    Given an initial starting point (and condition), the Monge maps are applied to
+    predict the next time points.
+
+    Args:
+        monge_maps (dict): Dictionary containing the Monge maps between marginals.
+        X_test (np.ndarray): Test data. Shape (n_samples, n_timepoints, n_features).
+        y_test (np.ndarray): Test labels. Shape (n_samples, n_timepoints).
+        t_test (np.ndarray): Test time points.
+        conditional (bool): Whether to use conditional OT maps.
+        interpolate (bool): Whether to interpolate missing time points.
+    """
+    X_test_hat = np.nan * np.ones(
+        shape=(X_test.shape[0], len(monge_maps) + 1, X_test.shape[2])
+    )
+    X_test_hat[:, 0] = X_test[:, 0]
+
+    if conditional:
+        idx_classes = y_test[:, 0]
+        all_timesteps = list(np.unique(t_test))
+        ys = [int(x) for x in np.unique(y_test)]
+
+        for c in ys:
+            for k, time in enumerate(monge_maps.keys()):
+                if monge_maps[time][c] is None:
+                    continue
+
+                skipped_timesteps = []
+                for next_existing_timestep in all_timesteps[k + 1 :]:
+                    nearest_dict = monge_maps.get(
+                        next_existing_timestep,
+                        monge_maps[
+                            min(
+                                monge_maps.keys(),
+                                key=lambda k: abs(k - next_existing_timestep),
+                            )
+                        ],
+                    )
+                    if nearest_dict.get(c, None) is not None:
+                        break
+                    else:
+                        skipped_timesteps.append(next_existing_timestep)
+
+                if len(skipped_timesteps) == len(all_timesteps[k + 1 :]):
+                    next_existing_timestep = 1.0
+                    skipped_timesteps = skipped_timesteps[:-1]
+
+                forward_prediction = (
+                    monge_maps[time][c]
+                    .transform(X_test_hat[:, k][idx_classes == c])
+                    .squeeze()
+                )
+                X_test_hat[
+                    idx_classes == c, all_timesteps.index(next_existing_timestep)
+                ] = forward_prediction
+
+                # Fill all missing columns with interpolation
+                if interpolate:
+                    for skipped_timestep in skipped_timesteps:
+                        before = all_timesteps[
+                            all_timesteps.index(skipped_timestep) - 1
+                        ]
+                        after = all_timesteps[all_timesteps.index(skipped_timestep) + 1]
+                        data_before = X_test_hat[
+                            idx_classes == c, all_timesteps.index(before)
+                        ]
+                        data_after = X_test_hat[
+                            idx_classes == c, all_timesteps.index(after)
+                        ]
+                        interpolation = data_before + (data_after - data_before) / (
+                            after - before
+                        ) * (skipped_timestep - before)
+                        X_test_hat[
+                            idx_classes == c, all_timesteps.index(skipped_timestep)
+                        ] = interpolation
+
+    else:
+        for k, time in enumerate(monge_maps.keys()):
+            X_test_hat[:, k + 1] = (
+                monge_maps[time].transform(X_test_hat[:, k]).squeeze()
+            )
+
+    return X_test_hat
+
+
+def train_fsi_model(
+    X_train, X_test, y_train=None, y_test=None, conditional=False, unbalanced=False
+):
+    """Wrapper function to train a FSI model.
+
+    Also returns the trajectory of the test data.
+
+    Args:
+        X_train (np.ndarray): Training data. Shape (n_samples, n_timepoints, n_features).
+        X_test (np.ndarray): Test data. Shape (n_samples, n_timepoints, n_features).
+        y_train (np.ndarray): Training labels. Shape (n_samples, n_timepoints).
+        y_test (np.ndarray): Test labels. Shape (n_samples, n_timepoints).
+        conditional (bool): Whether to use conditional OT maps.
+        unbalanced (bool): Whether to use unbalanced OT maps.
+    """
+    monge_maps = {}
+    X_test_hat = {}
+
+    # Learn Monge maps between marginals
+    # Barycentric Mapping
+    for k in range(len(X_train) - 1):
+        if not unbalanced:
+            if conditional:
+                ot_emd = ot.da.EMDTransport(limit_max=1e6)
+                ot_emd.fit(
+                    Xs=X_train[k], Xt=X_train[k + 1], ys=y_train[k], yt=y_train[k + 1]
+                )
+                monge_maps[k] = ot_emd
+            else:
+                ot_emd = ot.da.EMDTransport()
+                ot_emd.fit(Xs=X_train[k], Xt=X_train[k + 1])
+                monge_maps[k] = ot_emd
+        else:
+            if conditional:
+                raise NotImplementedError(
+                    "Unbalanced OT not implemented for conditional OT"
+                )
+            else:
+                a, b = (
+                    np.ones(X_train[k].shape[0]) / X_train[k].shape[0],
+                    np.ones(X_train[k + 1].shape[0]) / X_train[k + 1].shape[0],
+                )
+                M = ot.dist(X_train[k], X_train[k + 1])
+                M /= M.max()
+                reg = 0.005
+                reg_m_kl = 0.05
+                reg_m_l2 = 50
+                mass = 0.7
+                entropic_kl_uot = ot.unbalanced.mm_unbalanced(
+                    a, b, M, reg_m_kl, div="kl"
+                )
+                # entropic_kl_uot = ot.partial.partial_wasserstein(a, b, M, m=0.7)
+                monge_maps[k] = entropic_kl_uot
+
+    # Predict next time step for sample
+    for k in range(len(monge_maps) + 1):
+        if k == 0:
+            X_test_hat[0] = X_test[0]
+        else:
+            if not unbalanced:
+                X_test_hat[k] = monge_maps[k - 1].transform(X_test_hat[k - 1]).squeeze()
+            else:
+                n_samples = X_train[0].shape[0]
+                n_features = X_train[0].shape[1]
+                # Find closes point in training data and use it's mapping
+                X_test_hat[k] = np.zeros((n_samples, n_features))
+                for sample in range(n_samples):
+                    # Find closest point in training data at time point k-1
+                    closest_train_point = np.argmin(
+                        np.linalg.norm(
+                            X_train[k - 1] - X_test_hat[k - 1][sample], axis=1
+                        )
+                    )
+                    new_target_sample = np.argmin(
+                        monge_maps[k - 1][closest_train_point]
+                    )
+                    X_test_hat[k][sample] = X_train[k][new_target_sample]
+
+    return X_test_hat, monge_maps
+
+
+# Plots results on test data
+def plot_fsi_results(
+    df, X_hat, y, idx_plot, title, n_distribution, n_classes, figsize=(10, 5)
+):
+    """Plot results of FSI model training."""
+    fig, ax = plt.subplots(1, n_classes, figsize=figsize, sharex=True, sharey=True)
+    if n_classes == 1:
+        ax = [ax]
+
+    # Plat background samples
+    for k in range(n_classes):
+        sns.scatterplot(
+            data=df,
+            x="x",
+            y="y",
+            hue="target",
+            ax=ax[k],
+            legend=False,
+            alpha=0.15,
+            palette="tab10",
+        )
+        if n_classes > 1:
+            ax[k].set_title(f"c={k + 1}")
+
+    # Plot interpolations
+    xs = np.array([x / (n_distribution - 1) for x in range(n_distribution)])
+    for n in idx_plot:
+        ys = np.array([X_hat[k][n] for k in range(n_distribution)])
+        cs = CubicSpline(xs, ys)
+        spline = [cs(timepoint) for timepoint in np.linspace(0, 1, 100)]
+        color = "black"
+        c = 0 if n_classes == 1 else y[0][n] - 1
+        ax[c].scatter(ys[:, 0], ys[:, 1], color=color, label="Interpolated", s=50)
+        ax[c].plot(
+            [s[0] for s in spline],
+            [s[1] for s in spline],
+            color=color,
+            linestyle=":",
+            lw=2.5,
+            label="Interpolated",
+        )
+    plt.suptitle(title)
+    plt.tight_layout()
+    plt.show()
